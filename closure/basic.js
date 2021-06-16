5fab85d3ec62e4f84e97290c74cfc95b3e1b94c7
Navin Ojha <navinojha1996@gmail.com>
2021-06-16 11:08:38 +0530

    Added comment

--- closure/basic.js
+++ closure/basic.js
@@ -3,6 +3,7 @@
 
 A closure is a combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). In other words, a closure gives you access to an outer function’s scope from an inner function. In JavaScript, closures are created every time a function is created, at function creation time.
 
+THis is a for testing purpose
 */
 
 // Example
